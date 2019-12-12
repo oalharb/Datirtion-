8fdf64254f7df9a77d1d369b67505e6e124362ba
Mansour Aljuaid <aljuaidm@gmail.com>
2019-12-12 13:42:12 +0300

    Bingo!

---
+++ Box_detection_2/keywords.py
@@ -0,0 +1,88 @@
+from attrdict import AttrDict
+
+# Label keywords correspond to the words found on
+# the physical labels themselves.
+label_keywords = {
+    'serving_size': 'Serving Size',
+    'calories': 'Calories',
+    'energy': 'Energy',
+    'total_fat': 'Total Fat',
+    'saturated_fat': 'Saturated Fat',
+    'trans_fat': 'Trans Fat',
+    'poly_fat': 'Polyunsaturated Fat',
+    'mono_fat': 'Monounsaturated Fat',
+    'cholesterol': 'Cholesterol',
+    'sodium': 'Sodium',
+    'potassium': 'Potassium',
+    'carbohydrates': 'Total Carbohydrate',
+    'fiber': 'Dietary Fiber',
+    'sugars': 'Sugars',
+    'protein': 'Protein'
+}
+
+
+# Json keywords correspond to the keys used in code.
+json_keywords = {
+    'serving_size': 'Serving Size',
+    'calories': 'calories',
+    'energy': 'Energy',
+    'total_fat': 'total_fat',
+    'protein': 'protein',
+    'carbohydrates': 'carbohydrates',
+    'trans_fat': 'trans_fat',
+    'poly_fat': 'poly_fat',
+    'mono_fat': 'mono_fat',
+    'saturated_fat': 'saturated_fat',
+    'cholesterol': 'cholesterol',
+    'sodium': 'sodium',
+    'potassium': 'potassium',
+    'fiber': 'fiber',
+    'sugars': 'sugars'
+}
+
+# First pass at categorization
+#    'blurry': 'blurry',
+#    'wrinkled': 'wrinkled',
+#    'shadowed': 'shadowed',
+#    'skewed': 'skewed',
+#    'curved': 'curved',
+#    'not_bw': 'not_bw',
+
+type_keywords = {
+    'color': 'color',
+    'skewed': 'skewed',
+    'curved': 'curved',
+    'lighting': 'lighting',
+    'horizontal': 'horizontal'
+}
+
+# The Keywords object can be used to access both sets of keywords.
+Keywords = {
+    'json': json_keywords,
+    'label': label_keywords,
+    'spec_types': type_keywords,
+    'include': 'include',
+    'standard': 'standard'
+}
+
+# Make the Keywords object attribute-accessible
+# e.g. Keywords.label.calories == 'Calories'
+Keywords = AttrDict(Keywords)
+
+Keywords.ordered = [
+    Keywords.label.serving_size,
+    Keywords.label.calories,
+    Keywords.label.energy,
+    Keywords.label.total_fat,
+    Keywords.label.saturated_fat,
+    Keywords.label.trans_fat,
+    Keywords.label.poly_fat,
+    Keywords.label.mono_fat,
+    Keywords.label.cholesterol,
+    Keywords.label.sodium,
+    Keywords.label.potassium,
+    Keywords.label.carbohydrates,
+    Keywords.label.fiber,
+    Keywords.label.sugars,
+    Keywords.label.protein
+]
---
+++ Box_detection_2/label.py
@@ -0,0 +1,68 @@
+from keywords import Keywords
+
+
+class Label:
+    """
+    Label class contains all nutritional information corresponding
+    to a food label.
+    """
+    def __init__(self, attr_list=None, keyword_map=None):
+        # TODO: Clean up this method
+        attrs = list(Keywords.json.values())
+        attrs.extend(['name'])
+        attrs.extend(Keywords.spec_types.values())
+
+        true_false_attrs = Keywords.spec_types.values()
+
+        # Initialize all to None/False
+        for attr in attrs:
+            if attr in true_false_attrs:
+                setattr(self, attr, False)
+            else:
+                setattr(self, attr, None)
+
+        if attr_list is None:
+            attr_list = [None]*len(attrs)
+
+        def parse_attr_val(attr, val):
+            # Special type attrs are True/False
+            if attr in true_false_attrs:
+                def processTF(tfstring):
+                    if tfstring == "Yes":
+                        return True
+                    else:
+                        return False
+
+                return processTF(val)
+            elif val == "":
+                return None
+
+            return val
+
+        for i, attr in enumerate(attrs):
+            attr_list[i] = parse_attr_val(attr, attr_list[i])
+            setattr(self, attr, attr_list[i])
+
+        if keyword_map is not None:
+            for attr in attrs:
+                key = attr
+                if key in Keywords.label:
+                    key = Keywords.label[attr]
+
+                if key in keyword_map:
+                    amount = parse_attr_val(attr, keyword_map[key])
+                else:
+                    amount = None
+
+                setattr(self, attr, amount)
+
+    def __str__(self):
+        # Only print nutritional info
+        print_attrs = Keywords.json.values()
+        return str({k: self[k] for k in print_attrs})
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
